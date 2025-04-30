1a9bf4c00d3e14047b424ba6511d8269ad267657
alfimiftanur <alfinurhakim1234@gmail.com>
2025-04-30 19:04:37 +0700

    update at main class

--- pertemuan9/src/tugas/inheritanceMain.java
+++ pertemuan9/src/tugas/inheritanceMain.java
@@ -3,22 +3,27 @@ package tugas;
 public class inheritanceMain {
 //    Tugas latihan 1
     public static void main(String[] args){
+//        System.out.println("===== Tugas 1 =====");
 //        Karyawan kar = new Karyawan();
 //        kar.nama = "Jaja";
 //        System.out.println("Nama: "+ kar.getNama());
 
 //        tugas latihan 2
+
+//        System.out.println("===== Tugas 2 =====");
 //        Karyawan kar = new Karyawan();
 //        kar.nama = "Jaja";
 //        System.out.println("Nama: "+ kar.getNama() + "\nAlamat : "+ kar.getAlamat());
 
 //          tugas latihan 3
+//          System.out.println("===== Tugas 3 =====");
 //        Karyawan kar = new Karyawan("Jaja", "Bandung");
 //        System.out.println("Nama: "+ kar.getNama() + "\nAlamat : "+ kar.getAlamat());
 
 //        tugas latihan 4
+          System.out.println("===== Tugas 4 =====");
           Karyawan kar = new Karyawan("Jaja", "Bandung", 1000000);
-          System.out.println("Nama" + kar.getNama()+ "\nAlamat: " + kar.getAlamat()+ "\nGaji: " + kar.getGaji()) ;
+          System.out.println("Nama: " + kar.getNama()+ "\nAlamat: " + kar.getAlamat()+ "\nGaji: Rp " + kar.getGaji()) ;
 
     }
 }
